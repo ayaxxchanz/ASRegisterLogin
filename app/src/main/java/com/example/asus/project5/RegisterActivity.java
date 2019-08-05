ecfda3fb083d47a6a75c3989e0ce438b4372e0d5
ayaxxchanz <aliya.genius@gmail.com>
2019-08-06 00:13:54 +0800

    Letak field username untuk Register form then masuk dalam database Firebase.

--- app/src/main/java/com/example/asus/project5/RegisterActivity.java
+++ app/src/main/java/com/example/asus/project5/RegisterActivity.java
@@ -18,11 +18,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private EditText password, email;
+    private EditText password, email, cpassword, username;
     private Button button_register;
     private TextView signin;
     boolean doubleTap = false;
@@ -34,8 +39,11 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        username =(EditText) findViewById(R.id.signup_username);
         email = (EditText) findViewById(R.id.signup_email_input);
         password =(EditText) findViewById(R.id.signup_password_input);
+        cpassword =(EditText) findViewById(R.id.signup_cpass);
+
         button_register = (Button)findViewById(R.id.button_register);
         mAuth = FirebaseAuth.getInstance();
 
@@ -64,11 +72,20 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
     public void  createAccount(){
-        String Email = email.getText().toString().trim();
-        String Password = password.getText().toString().trim();
-        if (TextUtils.isEmpty(Email) || TextUtils.isEmpty(Password)){
+        final String Email = email.getText().toString().trim();
+        final String Password = password.getText().toString().trim();
+        final String Username = username.getText().toString().trim();
+        String CPassword = cpassword.getText().toString().trim();
+        //Check if the field are not empty
+        if (TextUtils.isEmpty(Email) || TextUtils.isEmpty(Password) || TextUtils.isEmpty(Username) || TextUtils.isEmpty(CPassword)){
             mProgressBar.setVisibility(View.GONE);
             Toast.makeText(this, "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //Check if Password = Confirm Password
+        if (!Password.equals(CPassword)){
+            mProgressBar.setVisibility(View.GONE);
+            Toast.makeText(this, "Password does not match.", Toast.LENGTH_SHORT).show();
             return;
         }
         mAuth.createUserWithEmailAndPassword(Email, Password)
@@ -80,11 +97,25 @@ public class RegisterActivity extends AppCompatActivity {
                             mProgressBar.setVisibility(View.GONE);
                             if (task.isSuccessful()) {
                                 //User is successfully registered and logged in
-                                //start Profile Activity here
-                                Toast.makeText(RegisterActivity.this, "Registration successful.",
-                                        Toast.LENGTH_SHORT).show();
-                                finish();
-                                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                                User user = new User(Username,Email,Password);
+                                FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        mProgressBar.setVisibility(View.GONE);
+                                        if(task.isSuccessful()){
+                                            //start Profile Activity here
+                                            Toast.makeText(RegisterActivity.this, "Registration successful.",
+                                                    Toast.LENGTH_SHORT).show();
+                                            finish();
+                                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                                        }
+                                        else{
+                                            Toast.makeText(RegisterActivity.this, task.getException().getMessage(),
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
                             }else{
                                 Toast.makeText(RegisterActivity.this, task.getException().getMessage(),
                                         Toast.LENGTH_SHORT).show();
